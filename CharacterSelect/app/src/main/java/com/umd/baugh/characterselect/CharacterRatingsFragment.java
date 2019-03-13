b82411a85ba1d37f61535d5aa7d648b3c7d939b4
mattj241 <londonm241@gmail.com>
2019-03-13 15:19:08 -0400

    Arthimetic working

--- CharacterSelect/app/src/main/java/com/umd/baugh/characterselect/CharacterRatingsFragment.java
+++ CharacterSelect/app/src/main/java/com/umd/baugh/characterselect/CharacterRatingsFragment.java
@@ -15,58 +15,137 @@ import android.widget.Toast;
 
 public class CharacterRatingsFragment extends Fragment implements RatingBar.OnRatingBarChangeListener {
     RatingBar strength, intellect, wisdom, dexterity;
-    TextView Title;
+    TextView Title, PointsText;
     String CharacterName;
-    String appName = "com.umd.baugh.characterselect",
-            warrior = "warrior", mage = "mage", healer = "healer",
-            hunter = "hunter", paladin = "paladin", strengthString = "strength",
+    String appName = "com.umd.baugh.characterselect", strengthString = "strength",
             intellectString = "intellect", wisdomString = "wisdom", dexterityString = "dexterity";
     SharedPreferences prefs;
-    int pointsUsed = 10;
+    static int MAX_POINTS= 10;
+    int lastStrengthRating, newStrengthRating,
+        lastWisdomRating, newWisdomRating,
+        lastIntellectRating, newIntellectRating,
+        lastDexterityRating, newDexterityRating;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        prefs = ((MainActivity)getActivity()).getSharedPreferences(
-                 appName, Context.MODE_PRIVATE);
+        prefs = getActivity().getSharedPreferences(appName, Context.MODE_PRIVATE);
+        lastStrengthRating = prefs.getInt(String.format("%s/%s", CharacterName, strengthString), 0);
+        lastWisdomRating = prefs.getInt(String.format("%s/%s", CharacterName, wisdomString), 0);
+        lastIntellectRating = prefs.getInt(String.format("%s/%s", CharacterName, intellectString), 0);
+        lastDexterityRating = prefs.getInt(String.format("%s/%s", CharacterName, dexterityString), 0);
+        int total = getRatingsTotal(lastStrengthRating, lastWisdomRating, lastIntellectRating, lastWisdomRating);
+
         CharacterName = this.getArguments().getString("characterType");
         View view = inflater.inflate(R.layout.ratings_page, container, false);
         Title = view.findViewById(R.id.CharatcterTitle);
         Title.setText(CharacterName);
+        PointsText = view.findViewById(R.id.PointsText);
+        PointsText.setText(String.valueOf(MAX_POINTS - total));
+
         strength = view.findViewById(R.id.StrengthRtgBar);
         intellect = view.findViewById(R.id.IntellectRtgBar);
         wisdom = view.findViewById(R.id.WisdomRtgBar);
         dexterity = view.findViewById(R.id.DexterityRtgBar);
+
+        strength.setRating(lastStrengthRating);
+        intellect.setRating(lastIntellectRating);
+        wisdom.setRating(lastWisdomRating);
+        dexterity.setRating(lastDexterityRating);
+
         strength.setOnRatingBarChangeListener(this);
         intellect.setOnRatingBarChangeListener(this);
         wisdom.setOnRatingBarChangeListener(this);
         dexterity.setOnRatingBarChangeListener(this);
+
         return view;
+    }
+
+    public int getRatingsTotal(int strength, int wisdom, int intellect, int dexterity){
+        int total = strength + wisdom + intellect + dexterity;
+        return total;
     }
 
     @Override
     public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
         String key = null;
-        int newStarAmount;
+        int localTotal = 0;
+        int newStarAmount = Math.round(ratingBar.getRating());
         switch (ratingBar.getId()){
             case R.id.StrengthRtgBar:
+                newStrengthRating = newStarAmount;
+                localTotal = getRatingsTotal(newStrengthRating, lastWisdomRating,
+                        lastIntellectRating, lastDexterityRating);
+                if (localTotal > MAX_POINTS){
+                    Toast.makeText(this.getContext(),
+                            String.format("that %s rating exceeds the player limit of 10", strengthString),
+                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    lastStrengthRating = newStrengthRating;
+                }
+                localTotal = getRatingsTotal(lastStrengthRating, lastWisdomRating,
+                        lastIntellectRating, lastDexterityRating);
+                ratingBar.setRating(lastStrengthRating);
                 key = String.format("%s/%s", CharacterName, strengthString);
                 break;
             case R.id.WisdomRtgBar:
+                newWisdomRating = newStarAmount;
+                localTotal = getRatingsTotal(lastStrengthRating, newWisdomRating,
+                        lastIntellectRating, lastDexterityRating);
+                if (localTotal > MAX_POINTS){
+                    Toast.makeText(this.getContext(),
+                            String.format("that %s rating exceeds the player limit of 10", wisdomString),
+                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    lastWisdomRating = newWisdomRating;
+                }
+                localTotal = getRatingsTotal(lastStrengthRating, lastWisdomRating,
+                        lastIntellectRating, lastDexterityRating);
+                ratingBar.setRating(lastWisdomRating);
                 key = String.format("%s/%s", CharacterName, wisdomString);
                 break;
             case R.id.IntellectRtgBar:
+                newIntellectRating = newStarAmount;
+                localTotal = getRatingsTotal(lastStrengthRating, lastWisdomRating,
+                        newIntellectRating, lastDexterityRating);
+                if (localTotal > MAX_POINTS){
+                    Toast.makeText(this.getContext(),
+                            String.format("that %s rating exceeds the player limit of 10", intellectString),
+                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    lastIntellectRating = newIntellectRating;
+                }
+                localTotal = getRatingsTotal(lastStrengthRating, lastWisdomRating,
+                        lastIntellectRating, lastDexterityRating);
+                ratingBar.setRating(lastIntellectRating);
                 key = String.format("%s/%s", CharacterName, intellectString);
                 break;
             case R.id.DexterityRtgBar:
+                newDexterityRating = newStarAmount;
+                localTotal = getRatingsTotal(lastStrengthRating, lastWisdomRating,
+                        lastIntellectRating, newDexterityRating);
+                if (localTotal > MAX_POINTS){
+                    Toast.makeText(this.getContext(),
+                            String.format("that %s rating exceeds the player limit of 10", dexterityString),
+                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    lastDexterityRating = newDexterityRating;
+                }
+                localTotal = getRatingsTotal(lastStrengthRating, lastWisdomRating,
+                        lastIntellectRating, lastDexterityRating);
+                ratingBar.setRating(lastDexterityRating);
                 key = String.format("%s/%s", CharacterName, dexterityString);
                 break;
             default:
-                Toast.makeText(this.getContext(), "Uh-oh", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this.getContext(), "Uh-oh", Toast.LENGTH_LONG).show();
                 break;
         }
-        newStarAmount = ratingBar.getNumStars();
         prefs.edit().putInt(key, newStarAmount).apply();
-        
+        String newTotal = String.valueOf(MAX_POINTS - localTotal);
+        PointsText.setText(newTotal);
     }
 }
